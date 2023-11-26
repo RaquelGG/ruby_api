57537d4f04e216b48554bb9a7539373892691e4e
RaquelGG <raquelgutierrezg.97@gmail.com>
2023-11-26 21:46:16 +0100

    remove random test code

--- app/resources/location_resource.rb
+++ app/resources/location_resource.rb
@@ -1,7 +1,3 @@
 class LocationResource < JSONAPI::Resource
     attributes :host, :geolocation
-
-    def self.find_by_keys(keys, context)
-        puts "test----------------------------------------------------------------------------"
-    end
 end
